b334a26beb8555eb37cb6026f67dca2c2cac6025
davidgaroro <davidgaroro@gmail.com>
2018-08-06 12:44:49 +0200

    Added pwa workbox options to cache google fonts

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const manifestJSON = require('./public/manifest.json')
+
+module.exports = {
+  pwa: {
+    themeColor: manifestJSON.theme_color,
+    workboxOptions: {
+      runtimeCaching: [{
+        urlPattern: new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+        handler: 'cacheFirst',
+        options: {
+          cacheName: 'google-fonts',
+          expiration: {
+            maxEntries: 30
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }]
+    }
+  }
+}
