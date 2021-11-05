81224544bf01a102e95f48e0703e2ac78c99ccb9
davidgaroro <davidgaroro@gmail.com>
2021-11-05 04:11:00 +0100

    Add files to production

---
+++ precache-manifest.e5feccb234f504599838f3b3c5b98312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a8bce7abed080d90261",
+    "url": "/vuetify-todo-pwa/css/app.27fd17c9.css"
+  },
+  {
+    "revision": "6a8bce7abed080d90261",
+    "url": "/vuetify-todo-pwa/js/app.b0f98876.js"
+  },
+  {
+    "revision": "bb891a118ef475b1b763",
+    "url": "/vuetify-todo-pwa/css/chunk-vendors.d3d186f7.css"
+  },
+  {
+    "revision": "bb891a118ef475b1b763",
+    "url": "/vuetify-todo-pwa/js/chunk-vendors.38942585.js"
+  },
+  {
+    "revision": "5efb9bdf865c1a93ddeb3a9d504b184e",
+    "url": "/vuetify-todo-pwa/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vuetify-todo-pwa/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,31 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vuetify-todo-pwa/precache-manifest.e5feccb234f504599838f3b3c5b98312.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vuetify-todo-pwa"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/, workbox.strategies.cacheFirst({ "cacheName":"google-fonts", plugins: [new workbox.expiration.Plugin({"maxEntries":30,"purgeOnQuotaError":false}), new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
